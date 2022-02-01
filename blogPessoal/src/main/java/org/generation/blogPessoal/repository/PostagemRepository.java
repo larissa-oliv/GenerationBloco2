01c101831d738b94c9e2040aa10b41e2c40c5b71
Larissa Oliveira <85649294+larissa-oliv@users.noreply.github.com>
2022-02-01 16:28:20 -0300

    Delete blogPessoal directory

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++
@@ -1,9 +0,0 @@
-package org.generation.blogPessoal.repository;
-
-import org.generation.blogPessoal.model.*;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.*;
-
-@org.springframework.stereotype.Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-}
